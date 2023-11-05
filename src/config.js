5225976c18c7d652c3da3a2566cf3aec6a21f49b
Devika Sugathan <devikasugathan@Devikas-MacBook-Air.local>
2023-11-06 00:07:18 +0100

    udt1

--- src/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
- 
-    apikey : "AIzaSyDeK9l8c_2ckNG4-DxwDsq6OmEBDqPYcfo",
-    
-    gembedurl : "https://www.google.com/maps/embed/v1/directions?key="
-    
-   };
-
-export {config};
